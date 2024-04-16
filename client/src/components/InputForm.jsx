0b9340f105aec1838105157a04ee9374f6ebdc2e
Subhajit <subhabera211@gmail.com>
2024-04-16 21:21:03 +0530

    adding-emoji

---
+++ client/src/components/InputForm.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import { AiOutlineSend } from "react-icons/ai";
+import Picker from '@emoji-mart/react'
+import data from '@emoji-mart/data';
+
+import { MdEmojiEmotions } from "react-icons/md";
+
+const InputForm = () => {
+
+    const [isPickerVisible, setPicerVisible] = useState(false);
+    const [message, setMessage] = useState('');
+
+
+    return (
+        <div className='input-form'>
+            <div className='emoji-picker-box'>
+                <MdEmojiEmotions as button className='emoji-icon' onClick={() => setPicerVisible(!isPickerVisible)} size='3rem' />
+                <div className={isPickerVisible ? 'd-block' : 'd-none'}>
+                    <Picker data={data} previewPosition="none" onEmojiSelect={(e) => {
+                        setMessage(message + e.native)
+                    }} />
+                </div>
+            </div>
+            <form className='form'>
+                <input
+                    type="text"
+                    value={message}
+                    onChange={(e) => { setMessage(e.target.value) }}
+                    placeholder="Enter a message.."
+                />
+                {/* <button type="submit" className='send-button'>
+                    <AiOutlineSend size={20} />
+                </button> */}
+
+                <AiOutlineSend as button size='3rem' />
+            </form>
+        </div>
+    )
+}
+
+export default InputForm
